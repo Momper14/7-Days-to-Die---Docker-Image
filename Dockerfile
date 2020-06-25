5050e7d8943cc2d28d5ee68ab04e0861846a02e0
momper14 <moritz.momper@gmail.com>
2020-06-25 12:15:28 +0200

    improved pre_build with error handling and Dockerfile for modifying original server starter and ability to modify server configs

--- Dockerfile
+++ Dockerfile
@@ -13,14 +13,14 @@ USER 7days
 
 # add data
 ADD --chown=7days:7days data/7daysded /7daysded
-ADD --chown=7days:7days startserver.sh /7daysded/startserver.sh
 ADD --chown=7days:7days data/Mods /7daysded/Mods
+RUN sed 's|-logfile.*.txt|-logfile /7daysded/logs/output_log__`date +%Y-%m-%d__%H-%M-%S`.txt|' /7daysded/startserver.sh >> /7daysded/tmp && mv /7daysded/tmp /7daysded/startserver.sh && chmod +x /7daysded/startserver.sh
 RUN mkdir -p /7daysded/logs /7daysded/.local/share/7DaysToDie
 ADD --chown=7days:7days data/7daysded/serverconfig.xml /7daysded/.local/share/7DaysToDie/serverconfig.xml
 
 WORKDIR /7daysded
 
-VOLUME ["/7daysded/Mods", "/7daysded/.local/share/7DaysToDie"]
+VOLUME ["/7daysded/Mods", "/7daysded/.local/share/7DaysToDie", "/7daysded/logs", "/7daysded/Data/Config"]
 
 # remote
 EXPOSE 8080-8081/tcp
--- pre_build.sh
+++ pre_build.sh
@@ -1,19 +1,25 @@
-apt-get install wget lib32gcc1
+function error_exit(){
+    print $@
+    exit 1
+}
 
+apt-get install wget lib32gcc1 || error_exit "could not install debian packages"
+
+[[ -d data ]] || mkdir data || error_exit "cloud not create dir data"
 cd data
 
 if [ ! -f steamcmd.sh ]; then
-    wget http://media.steampowered.com/installer/steamcmd_linux.tar.gz
-    tar xvzf steamcmd_linux.tar.gz
+    wget http://media.steampowered.com/installer/steamcmd_linux.tar.gz || error_exit "failed to download steamcmd.sh"
+    tar xvzf steamcmd_linux.tar.gz || error_exit "failed to extract steam"
     rm steamcmd_linux.tar.gz
 fi
 
-./steamcmd.sh +login anonymous +force_install_dir ./7daysded +app_update 294420 validate +quit
+./steamcmd.sh +login anonymous +force_install_dir ./7daysded +app_update 294420 validate +quit || error_exit "failed to download game from steam"
 
 if [ -d "Mods" ]; then
-    rm -rf Mods
+    rm -rf Mods || error_exit "failed to remove Mods folder"
 fi
 
-wget http://illy.bz/fi/7dtd/server_fixes.tar.gz
-tar xvzf server_fixes.tar.gz
+wget http://illy.bz/fi/7dtd/server_fixes.tar.gz || error_exit "failed to downloaded Alloc's server fixes"
+tar xvzf server_fixes.tar.gz || "failed to extract Alloc's server fixes"
 rm server_fixes.tar.gz
